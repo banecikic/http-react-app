bc1f6e0931ee1d3938f1c8fefdfb76319e8f70ee
banecikic <branimir.cikic@gmail.com>
2020-11-23 19:15:45 +0100

    add more functionalities

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -8,25 +8,37 @@ class FullPost extends Component {
         loadedPost: null
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
+    componentDidUpdate() {
         if (this.props.id){
-            axios.get('http://jsonplaceholder.typicode.com/posts/' + this.props.id)
-                .then(response => {
-                    this.setState({})
-                    // console.log(response);
-                });
+            if( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)){
+                axios.get('http://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                    .then(response => {
+                        this.setState({loadedPost: response.data})
+                        // console.log(response);
+                    });
+            }
         }
+    }
+
+    deletePostHandler = () => {
+        axios.delete('http://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                console.log(response);
+            });
     }
 
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if (this.props.id){
+            post = <p style={{textAlign: 'center'}}>Loading...</p>
+        }
+        if (this.state.loadedPost){
             post = (
                 <div className="FullPost">
-                    <h1>{loadedPost.title}</h1>
-                    <p>{loadedPost.body}</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
 
